5b9a8389a6fbc7275efb72987441860ab28d43f1
noahb <noahb@emeraldaire.com>
2021-01-02 07:41:13 -0800

    Express With Routers

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const morgan = require('morgan');
+const campsiteRouter = require('./routes/campsiteRouter');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+//This will configure morgan to use the development version
+app.use(morgan('dev'));
+//Include the Express JSON parser to deal with relevant request body objects.
+app.use(express.json());
+
+//Create an assocation with the parent route and its router class.
+app.use('/campsites', campsiteRouter);
+
+//Set up the server object to deliver static files from the public folder.
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
